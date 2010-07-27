2213880748e724b6588a9eeaa210e167f312452f
repo sync <polytheus@googlemail.com>
2010-07-27 20:20:02 +0200

    Replace GoogleContactsProvider by ContactsProvider

--- openetna.mk
+++ openetna.mk
@@ -1,11 +1,50 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
-#
-# Overrides
+PRODUCT_PACKAGES := \
+ 	GoogleSubscribedFeedsProvider \
+	Stk \
+	Superuser \
+    framework-res \
+    Browser \
+    Contacts \
+    Launcher \
+    HTMLViewer \
+    Phone \
+    ApplicationsProvider \
+    ContactsProvider \
+	DownloadProvider \
+    GoogleSearch \
+    MediaProvider \
+    PicoTts \
+    SettingsProvider \
+    TelephonyProvider \
+    TtsService \
+    VpnServices \
+    UserDictionaryProvider \
+    PackageInstaller \
+    Bugreport \
+	AccountAndSyncSettings \
+    AlarmClock \
+    AlarmProvider \
+    Bluetooth \
+    Calculator \
+    Calendar \
+    Camera \
+    CertInstaller \
+    DrmProvider \
+    Email \
+    Gallery \
+    LatinIME \
+    Mms \
+    Music \
+    Settings \
+    Sync \
+    Updater \
+    CalendarProvider \
+    SyncProvider
+
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+
 PRODUCT_NAME := openetna
 PRODUCT_DEVICE := openetna
-
-PRODUCT_PACKAGES := GoogleContactsProvider \
-					GoogleSubscribedFeedsProvider \
-					Stk \
-					Superuser
-
+PRODUCT_BRAND := openetna
